300058f67c2bf85a95b4ccc3c2a00f04cb28f8c0
Sunisgoing <924117720112@njust.edu.cn>
2026-03-12 00:35:20 +0800

    Create prehead.h

---
+++ DATA STRUCTURE/LinearList/prehead.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//+++++ c++ system header file 
+#include <iostream>
+
+using namespace std; 
+
+//+++++ c system header file
+#include <string.h>
+
+//+++++self header file
+#include "prestruct.h"
+
+
