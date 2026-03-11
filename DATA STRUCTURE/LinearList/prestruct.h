685d84b68b108e6088e760e5f083f2315a07ea37
Sunisgoing <924117720112@njust.edu.cn>
2026-03-12 00:35:48 +0800

    Create prestruct.h

---
+++ DATA STRUCTURE/LinearList/prestruct.h
@@ -0,0 +1,14 @@
+#pragma once
+
+typedef char           int_8  ;
+typedef unsigned char  uint_8 ;
+typedef short int      int_16 ;
+typedef unsigned short uint_16;
+typedef int            int_32 ;
+typedef unsigned int   uint_32;
+
+
+struct Polyn{
+	double  factor;
+	uint_32   exponent;
+};
